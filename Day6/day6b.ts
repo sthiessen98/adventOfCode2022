3e3ec5b2e00941905e34a7c20019d884fe709691
spencer thiessen <spencer.thiessen98@hotmail.com>
2022-12-23 12:14:35 -0800

    Completed Day 6 part a and part b

---
+++ Day6/day6b.ts
@@ -0,0 +1,38 @@
+import { readFileSync, createReadStream } from 'fs';
+import * as rd from 'readline';
+
+var reader = rd.createInterface(createReadStream(require.resolve('./day6.txt')));
+
+let datastream: string = '';
+let answer: number = 0;
+let streamLength: number = 14;
+
+reader.on("line", (l: string) => {
+    datastream = l;
+});
+
+reader.on("close", ()=>{
+    let cont = true;
+    while(cont){
+        const result= checkDuplicates(datastream.slice(answer, answer + streamLength));
+        if(!result){
+            cont = false;
+            answer+= streamLength;
+        }else{
+            answer++;
+        }
+    }
+    console.log(`answer: ${answer}`);
+});
+
+function checkDuplicates(chars: String){
+    for(let x=0; x < chars.length; x++){
+        for(let y=x; y < chars.length; y++){
+            if(chars.charAt(x) === chars.charAt(y) && x !== y){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
